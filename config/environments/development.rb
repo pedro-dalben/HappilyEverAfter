c746298bfcaa68bb74392b1a6222a34a86009c16
Pedro Dalben <pedrodalbenmorais@gmail.com>
2025-03-24 09:49:23 -0300

    correçoes gerais deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,15 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   config.action_view.annotate_rendered_view_with_filenames = true
 
+  config.assets.debug = true
+  config.assets.compile = true
+  config.assets.digest = false
+
+  begin
+    require "hotwire/spark"
+  rescue LoadError
+  end
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
